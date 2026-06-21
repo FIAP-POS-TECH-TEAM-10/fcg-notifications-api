c52c9f7650125299b723c0bbe132c82dfbef0555
Daniel Gonçalves <daniel.bnu7@gmail.com>
2026-06-21 13:01:58 -0300

    refactor(api): stateless worker host with health check
    Drop EF Core, Swagger, JWT and MediatR wiring from Program.cs; register
    MassTransit and expose /health (k8s liveness). Remove EntityFrameworkCore.Design
    from the Api project.

--- app/src/Fiap.FCGames.Notifications.Api/Program.cs
+++ app/src/Fiap.FCGames.Notifications.Api/Program.cs
@@ -1,63 +1,31 @@
 using Fiap.FCGames.Notifications.CrossCutting.Extensions;
 using Fiap.FCGames.Notifications.CrossCutting.Middleware;
-using Fiap.FCGames.Notifications.Infra.DataProvider.Contexto;
-using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Host.UseSerilog();
 
 builder.Services.AddControllers();
 builder.Services.Configure<Microsoft.AspNetCore.Mvc.ApiBehaviorOptions>(options =>
     options.SuppressModelStateInvalidFilter = true);
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
-builder.Services.RegisterDI();
-builder.Services.AddMediatRConfiguration();
+builder.Services.AddHealthChecks();
 
-builder.Services.RegisterSwaggerGenerator();
-
-builder.Services.AddAutenticacaoApi(builder.Configuration);
-
-builder.Services.AddAutorizacaoApi();
-
-builder.Services.AddContextDatabase(builder.Configuration);
-
-Log.Logger = new LoggerConfiguration()
-    .ReadFrom.Configuration(builder.Configuration)  
-    .Enrich.FromLogContext()
-    .CreateLogger();
-
-builder.Host.UseSerilog();
+builder.Services.AddMassTransitRabbitMq(builder.Configuration);
 
 var app = builder.Build();
 
-// Aplica automaticamente as migrations pendentes e cria o banco SQLite local na inicialização.
-if (!app.Environment.IsEnvironment("Testing"))
-{
-    using var scope = app.Services.CreateScope();
-    var dbContext = scope.ServiceProvider.GetRequiredService<FcGamesContexto>();
-    dbContext.Database.Migrate();
-}
-
 app.UseCorrelationId();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.RegisterSwagger();
-    app.MapOpenApi();
-    app.RegisterScalar();
-}
 app.UseErrorHandlingMiddleware();
 
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
 app.MapControllers();
+app.MapHealthChecks("/health");
 
-app.Run();
+await app.RunAsync();
